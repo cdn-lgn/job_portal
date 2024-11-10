bbeb8470f70fb960e4bd6c291f4afc6b2f2cd223
cdn-lgn <logan2246t@gmail.com>
2024-11-10 17:20:07 +0530

    Resolved merge conflict in backend/index.js: Keep env variable and specify methods

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/store";
+import { store, persistor } from "./redux/store"; // Import persistor
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
 import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        {/* PersistGate to delay rendering until rehydration is complete */}
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
     <Toaster />
